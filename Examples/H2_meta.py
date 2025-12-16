ecaa65344dbf4fa8a37ceb6f0bc11763797b0209
Ben Thorpe <ben.thorpe@york.ac.uk>
2025-12-16 14:42:55 +0000

    added Meta OMAT24 models

--- Examples/H2_meta.py
+++ Examples/H2_meta.py
@@ -12,19 +12,21 @@ except:
 from ase.optimize import BFGS
 from ase.calculators.nwchem import NWChem
 from ase.io import write
+import numpy as np
+
 
 if __name__=='__main__':
-    # path to the compiled model, inside the MatterSim container
-    Compiled_Model_Path = 'MatterSim-v1.0.0-5M.pth'
-
-    # Setup the system with ASE, in this case a simple H2 molecule
-    h2 = Atoms('H2', positions=[[0, 0, 0],[0, 0, 0.7]])
+    d = 0.9575
+    t = np.pi / 180 * 104.51
+    # Setup the system with ASE, in this case a simple H2O molecule
+    h2 = Atoms('H2O',positions=[(d, 0, 0),(d * np.cos(t), d * np.sin(t), 0),(0, 0, 0)],)
 
     # Tell ASE to use MatterSim as a Calculator
-    h2.calc = initialise_model('MatterSim',path=Compiled_Model_Path)
+    h2.calc = initialise_model("esen-30m-oam")
 
     # Do the calculations 
-    opt = BFGS(h2)
-    opt.run(fmax=0.02)
-    write('H2.xyz', h2)
-    h2.get_potential_energy()
+    opt = BFGS(h2, trajectory='H2O.traj')
+    opt.run(fmax=0.01)
+    write('H2O.xyz', h2)
+    energy = h2.get_potential_energy()
+    print(f"potential = {energy} eV")
--- Examples/Matbench_Models.py
+++ Examples/Matbench_Models.py
@@ -4,7 +4,7 @@ def initialise_error(message:str):
     print(f'ERROR: {message}')
     sys.exit(23)
 
-def initialise_model(ML_model_option:str):
+def initialise_model(ML_model_option:str,path='/models'):
     '''
     Initialise the ML model, only need to do this once
     Params:
@@ -14,7 +14,15 @@ def initialise_model(ML_model_option:str):
 
     ML_model_option_lower=ML_model_option.lower()
     ASE_Calculator=None
-
+# # Dict of models and locations of corresponding checkpoint files.
+# # Note: if not using containers you will need to update these locations
+    models_directory='/models'
+    checkpoint_path = {'esen-30m-oam': f"{models_directory}/FairChem/esen_30m_oam.pt",
+                  'esen-30m-mp': f"{models_directory}/FairChem/esen_30m_mptrj.pt",
+                  'eqv2_m': f"{models_directory}/FairChem/eqV2_86M_omat_mp_salex.pt",
+                  'mattersim':f"{models_directory}/MatterSim-v1.0.0-5M.pth"
+                   }
+#Models using mattersim
     if ML_model_option_lower == 'mattersim':
 
         try:
@@ -23,8 +31,19 @@ def initialise_model(ML_model_option:str):
             initialise_error('MatterSim module cannot be found, please install MatterSim.')
         # path to the compiled model, Note: this should be already is inside 
         # the MatterSim container
-        Compiled_Model_Path = 'MatterSim-v1.0.0-5M.pth'
+        Compiled_Model_Path = checkpoint_path[ML_model_option_lower]
         ASE_Calculator = MatterSimCalculator(load_path=Compiled_Model_Path,device='cpu')
+
+# Models from Meta Ai (Facebook) all use fairchem
+    elif (ML_model_option_lower == 'esen-30m-oam' or ML_model_option_lower == 'eqv2_m' \
+           or ML_model_option_lower == 'esen-30m-mp'):
+        
+        # from fairchem.core import pretrained_mlip, FAIRChemCalculator
+        # predictor = pretrained_mlip.get_predict_unit("esen_30m_oam", device="cuda")
+        # ASE_Calculator = FAIRChemCalculator(predictor, task_name="omol")
+        from fairchem.core import OCPCalculator
+        ASE_Calculator = OCPCalculator(checkpoint_path="/root/.cache/huggingface/hub/models--facebook--OMAT24/snapshots/8a5a78c7ba7b250a17e85fe85943c4608499d895/esen_30m_oam.pt")
+
     else:
         initialise_error(f'Unknown module {ML_model_option}')
     return ASE_Calculator
