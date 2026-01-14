0c8a44ae90a9d54354e41baf6dec1e8a97d57754
Ben Thorpe <ben.thorpe@york.ac.uk>
2026-01-14 15:04:58 +0000

    added UMA and Omat24 models from Meta

--- Examples/Matbench_Models.py
+++ Examples/Matbench_Models.py
@@ -4,45 +4,122 @@ def initialise_error(message:str):
     print(f'ERROR: {message}')
     sys.exit(23)
 
-def initialise_model(ML_model_option:str,path='/models'):
+def set_argument_defaults(model_group:str,arguments:dict):
+        '''
+        Docstring for set_defaults
+        
+        :param 
+            model_group: string to indicate which group the model belongs to
+            arguments: 
+        '''
+        # default to cpu if not specified
+        if 'device' not in arguments:
+                arguments['device']='cpu'
+
+        if model_group=='Meta-UMAT':
+            # default to molecule if not specified
+            if 'task' not in arguments:
+                    arguments['device']='omol'
+
+def initialise_model(ML_model_option:str,models_dir='/models',**kwargs):
     '''
     Initialise the ML model, only need to do this once
     Params:
         ML_model_option:str - Name of ML toolkit to use
-        model_loc:str - Path to compiled ML model
+        kwargs:dict - options that are specific to the chosen model.
+                      See the docs or comments on specific models for
+                      what params are used.
     '''
+# dicts of valid model names and corresponding checkpoint files
+    Meta_OMat24 =  {
+            'esen-30m-oam': "esen_30m_oam.pt",
+            'esen-30m-mp': "esen_30m_mptrj.pt",
+            'eqv2_m': "eqV2_86M_omat_mp_salex.pt",
+            'eqv2-l-dens':'eqV2_dens_153M_mp.pt',
+            'eqv2-m-dens':'eqV2_dens_86M_mp.pt',
+            'eqv2-s-dens':'eqV2_dens_31M_mp.pt',
+            'eqv2-s':'eqV2_31M_mp.pt',
+            'eqv2-s-oam':'eqV2_31M_omat_mp_salex.pt',
+            'eqv2-m-oam':'eqV2_86M_omat_mp_salex.pt',
+            'esen-30m-omat':'esen_30m_omat.pt',
+            'eqv2-s-omat':'eqV2_31M_omat.pt',
+            'eqv2-m-omat':'eqV2_86M_omat.pt',
+            'eqv2-l-omat':'eqV2_153M_omat_mp_salex.pt'
+            }
+    
+    Meta_UMA = {
+            'uma-s-1.1':'uma-s-1p1.pt',
+            'uma-s-1':'uma-s-1.pt',
+            'uma-m-1.1':'uma-m-1p1.pt'
+            }
+    
+    MatterSim = {'mattersim':'MatterSim-v1.0.0-5M.pth'}
 
     ML_model_option_lower=ML_model_option.lower()
     ASE_Calculator=None
-# # Dict of models and locations of corresponding checkpoint files.
-# # Note: if not using containers you will need to update these locations
-    models_directory='/models'
-    checkpoint_path = {'esen-30m-oam': f"{models_directory}/FairChem/esen_30m_oam.pt",
-                  'esen-30m-mp': f"{models_directory}/FairChem/esen_30m_mptrj.pt",
-                  'eqv2_m': f"{models_directory}/FairChem/eqV2_86M_omat_mp_salex.pt",
-                  'mattersim':f"{models_directory}/MatterSim-v1.0.0-5M.pth"
-                   }
-#Models using mattersim
-    if ML_model_option_lower == 'mattersim':
+
+###############################################################################   
+#         MatterSim (Microsoft)
+#
+#         Params:
+#           "device" - what device to target.can be either 'cpu' or 'cuda'.
+###############################################################################
+    if ML_model_option_lower in MatterSim:
 
         try:
             from mattersim.forcefield import MatterSimCalculator
         except:
             initialise_error('MatterSim module cannot be found, please install MatterSim.')
-        # path to the compiled model, Note: this should be already is inside 
-        # the MatterSim container
-        Compiled_Model_Path = checkpoint_path[ML_model_option_lower]
-        ASE_Calculator = MatterSimCalculator(load_path=Compiled_Model_Path,device='cpu')
+            exit() 
 
-# Models from Meta Ai (Facebook) all use fairchem
-    elif (ML_model_option_lower == 'esen-30m-oam' or ML_model_option_lower == 'eqv2_m' \
-           or ML_model_option_lower == 'esen-30m-mp'):
+        Compiled_Model_Path = f"{models_dir}/MatterSim/{MatterSim[ML_model_option_lower]}"
+        ASE_Calculator = MatterSimCalculator(load_path=Compiled_Model_Path,device=kwargs['device'])
+###############################################################################
+#          Meta Ai (Facebook)
+###############################################################################
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#   Meta Open Materials 2024 (OMat24) Models:
+#
+#   Note: these use fairchem 1.10 and are incompatible with the latest version 
+#   (currently 2.13).
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    elif (ML_model_option_lower in Meta_OMat24):
         
-        # from fairchem.core import pretrained_mlip, FAIRChemCalculator
-        # predictor = pretrained_mlip.get_predict_unit("esen_30m_oam", device="cuda")
-        # ASE_Calculator = FAIRChemCalculator(predictor, task_name="omol")
         from fairchem.core import OCPCalculator
-        ASE_Calculator = OCPCalculator(checkpoint_path="/root/.cache/huggingface/hub/models--facebook--OMAT24/snapshots/8a5a78c7ba7b250a17e85fe85943c4608499d895/esen_30m_oam.pt")
+        
+        Compiled_Model_Path = Meta_OMat24[ML_model_option_lower]
+        ASE_Calculator = OCPCalculator(checkpoint_path=Compiled_Model_Path)
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# UMA: A Family of Universal Models for Atoms
+#
+# Meta's latest models (as of Jan 2026)
+# These use Fairchem 2.13 and have a completely redesigned syntax
+# 
+#       Params:
+#           "device" - what device to target. Can be either 'cpu' or 'cuda'.
+#           "workers" - Number of Gpus to use for inferencing
+#           "task" - Set the task for your application. 
+#                    This must be one of:
+#                       oc20: for catalysis
+#                       omat: for inorganic materials
+#                       omol: for molecules
+#                       odac: for MOFs
+#                       omc:  for molecular crystals
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    elif (ML_model_option_lower in Meta_UMA):
+         
+        from fairchem.core import pretrained_mlip, FAIRChemCalculator
+
+        # check kwargs are correct
+        if 'task' not in kwargs:
+             raise ValueError('Meta UMA model requires the input argument "task".')
+        # set no of gpus to use
+        if 'workers' not in kwargs:
+             kwargs['workers'] = 1
+        
+        predictor = pretrained_mlip.get_predict_unit(Meta_UMA[ML_model_option_lower], device=kwargs['device'])
+        ASE_Calculator = FAIRChemCalculator(predictor, task_name=kwargs['task'])
 
     else:
         initialise_error(f'Unknown module {ML_model_option}')
