0a4fc715c06980230e88d96592cfc3d202c4d74d
Ben Thorpe <ben.thorpe@york.ac.uk>
2026-01-14 16:24:53 +0000

    I ran the code through a linter to clean it up a bit

--- run_container.py
+++ run_container.py
@@ -4,35 +4,42 @@ import subprocess, sys, yaml
 from dataclasses import dataclass, field
 from typing import Optional, List
 from dacite import from_dict
-from check_yaml import DuplicateKeyDetector, DuplicateKeyError 
+from check_yaml import DuplicateKeyDetector, DuplicateKeyError
 from check_yaml import is_valid_name
 from check_URI import check_container_def
 from util_functions import create_build_options
 import logging
 
-logging.basicConfig(level=logging.INFO,filename='logs/log.log',filemode='w',
-                format="%(asctime)s - %(levelname)s - %(message)s")
+logging.basicConfig(
+    level=logging.INFO,
+    filename="logs/log.log",
+    filemode="w",
+    format="%(asctime)s - %(levelname)s - %(message)s",
+)
+
+
 @dataclass
 class ContainerConfig:
-    description: str 
+    description: str
     image_file: str = field(default="")
     container_definition: str = field(default="")
     encryption_key: str = field(default="")
     shared_directories: str = field(default="")
     group: str = field(default="None")
-    encrypted: bool= field(default=False)
+    encrypted: bool = field(default=False)
     registry: str = field(default="docker")
     read_only: bool = field(default=False)
     use_GPU: bool = field(default=True)
     sandbox: bool = field(default=False)
-    build_options:dict = field(default_factory=dict)
-    
+    build_options: dict = field(default_factory=dict)
+
+
 class CMD_FormatError(Exception):
     """
     Custom Exception to be raised when using an operation that
-    is not valid or that has not been implemented. This should 
-    not normally be raised as its handled in the command line 
-    options but this is here in case someone adds an option 
+    is not valid or that has not been implemented. This should
+    not normally be raised as its handled in the command line
+    options but this is here in case someone adds an option
     in the future but forgets to handle it in format_command.
 
     Attributes:
@@ -42,6 +49,7 @@ class CMD_FormatError(Exception):
     def __init__(self, message):
         self.message = message
         super().__init__(self.message)
+
 
 def check_container_config(config_files: list):
     """
@@ -88,7 +96,9 @@ def check_container_config(config_files: list):
             if result.container_definition == "":
                 result.container_definition = f"Definitions/{key}.def"
             else:
-                result.container_definition = check_container_def(result.container_definition)
+                result.container_definition = check_container_def(
+                    result.container_definition
+                )
 
             # do some checks for shared directory if defined
             if result.shared_directories != "":
@@ -101,9 +111,10 @@ def check_container_config(config_files: list):
                     err_msg = f"The shared directory {result.shared_directories} \n \
                     defined in {file.name} should be directory not a file."
                     raise FileNotFoundError(err_msg)
-        logging.info(f"{file.name} OK")      
+        logging.info(f"{file.name} OK")
     print(f"All config files look OK")
     return Containers
+
 
 def load_container_config_file(container_config):
     """
@@ -136,18 +147,20 @@ def load_container_config_file(container_config):
 
     return Containers
 
-def image_exists(image_file:str):
+
+def image_exists(image_file: str):
     if not Path(image_file).exists():
         msg = f"A container with the name {image_file} \
             \n could not be found please run build first."
         raise FileNotFoundError(msg)
     return
 
+
 def format_command(
     operation: str,
-    model_name:str, 
-    Container:ContainerConfig, 
-    cmd_list: List[str] = ["hostname"]
+    model_name: str,
+    Container: ContainerConfig,
+    cmd_list: List[str] = ["hostname"],
 ):
     """
     Function to create appropriate Apptainer command based on the
@@ -159,11 +172,11 @@ def format_command(
     # check for encryption and add appropriate flags
     if Container.encrypted:
         if Container.encryption_key != "":
-            enc_flag = ' --passkey '
+            enc_flag = " --passkey "
         else:
-            enc_flag = f' --pem-path {Container.encryption_key} '
+            enc_flag = f" --pem-path {Container.encryption_key} "
     else:
-        enc_flag = ''
+        enc_flag = ""
 
     if operation == "run":
         cmd = " ".join(cmd_list)
@@ -174,7 +187,9 @@ def format_command(
     elif operation == "build" or operation == "load":
         msg = "Building"
         build_options_str = create_build_options(Container.build_options)
-        apptainer_command = f"apptainer build{build_options_str} {enc_flag}{image} {definition}"
+        apptainer_command = (
+            f"apptainer build{build_options_str} {enc_flag}{image} {definition}"
+        )
 
     elif operation == "start":
         msg = "Starting"
@@ -189,122 +204,97 @@ def format_command(
     else:
         # this path should not happen but just in case.
         apptainer_command = ""
-        raise CMD_FormatError(f"{operation} is Not a valid operation," + \
-                               "This should not happen. Did you add an option" + \
-                               "and forget to update format_command?")
+        raise CMD_FormatError(
+            f"{operation} is Not a valid operation,"
+            + "This should not happen. Did you add an option"
+            + "and forget to update format_command?"
+        )
 
     print("*********************************************************************")
     print(f"***************** {msg}: {model_name} *********************")
     print("*********************************************************************")
     return apptainer_command
 
+
 def parse_cmd_arguments():
-    """ 
+    """
     Function to handle parsing of command line arguments
     """
 
     parser = argparse.ArgumentParser(
-        description="A CLI tool for easily running AI/ML containers on Bede.")
-        
+        description="A CLI tool for easily running AI/ML containers on Bede."
+    )
+
     # Subparser to create subcommands for each operation (run, build, load ect.)
     subparsers = parser.add_subparsers(
-        dest="operation",
-        required=True,
-        help="Operation to perform."
+        dest="operation", required=True, help="Operation to perform."
     )
     # sub-parser for the run operation
-    run_parser = subparsers.add_parser(
-        "run", 
-        help="Run command(s), with the Container")
+    run_parser = subparsers.add_parser("run", help="Run command(s), with the Container")
 
-    run_parser.add_argument(
-        "model_name",
-        type=str, 
-        help="Name of Model to use")
-    
-    run_parser.add_argument(
-        'cmd',
-        type=str,
-        nargs='+',
-        help="Command(s) to run")
-    
+    run_parser.add_argument("model_name", type=str, help="Name of Model to use")
+
+    run_parser.add_argument("cmd", type=str, nargs="+", help="Command(s) to run")
+
     # sub-parser for the build operation
     build_parser = subparsers.add_parser(
-        "build", 
-        help="Build the Container, exactly equivalent to load")
+        "build", help="Build the Container, exactly equivalent to load"
+    )
 
-    build_parser.add_argument(
-        "model_name",
-        type=str, 
-        help="Name of Model to use")
-     
+    build_parser.add_argument("model_name", type=str, help="Name of Model to use")
+
     # sub-parser for the load operation
     load_parser = subparsers.add_parser(
-        "load",
-        help="Build the Container, exactly equivalent to build")
+        "load", help="Build the Container, exactly equivalent to build"
+    )
 
-    load_parser.add_argument(
-        "model_name",
-        type=str, 
-        help="Name of Model to use")    
-    
+    load_parser.add_argument("model_name", type=str, help="Name of Model to use")
+
     # sub-parser for the list operation
-    list_parser = subparsers.add_parser(
-        "list",
-        help="List available containers")
+    list_parser = subparsers.add_parser("list", help="List available containers")
 
     list_parser.add_argument(
-        "--group",
-        type=str,
-        default='', 
-        help="optional group of containers to list")
-    
-# sub-parser for the start operation
+        "--group", type=str, default="", help="optional group of containers to list"
+    )
+
+    # sub-parser for the start operation
     start_parser = subparsers.add_parser(
-        "start", 
-        help="Start Container as background process")
+        "start", help="Start Container as background process"
+    )
 
-    start_parser.add_argument(
-        "model_name",
-        type=str, 
-        help="Name of Model to use")
-# sub-parser for the stop operation
+    start_parser.add_argument("model_name", type=str, help="Name of Model to use")
+    # sub-parser for the stop operation
     stop_parser = subparsers.add_parser(
-        "stop", 
-        help="Stop container that is running in the background")
+        "stop", help="Stop container that is running in the background"
+    )
 
-    stop_parser.add_argument(
-        "model_name",
-        type=str, 
-        help="Name of Model to use")
-    
-# other arguments for main parser
+    stop_parser.add_argument("model_name", type=str, help="Name of Model to use")
+
+    # other arguments for main parser
     parser.add_argument(
-        "--config_file", 
-        type=str, 
-        default=None, 
-        help="path to Config file for Models"
+        "--config_file", type=str, default=None, help="path to Config file for Models"
     )
     parser.add_argument(
         "--debug",
-        action='store_true',
+        action="store_true",
         help="Print generated Apptainer command instead of running container, useful for sanity checking",
     )
 
-    args =parser.parse_args()
+    args = parser.parse_args()
 
     if args.operation != "run":
-        args.cmd=""
+        args.cmd = ""
     return args
 
-def list_containers(Containers:dict,group:str=''):
+
+def list_containers(Containers: dict, group: str = ""):
     print("*************************************")
     print("Currently available containers:")
     print("*************************************")
     print(f"Name:         Group:    Description:")
-    print("-------------------------------------")  
+    print("-------------------------------------")
     for key, value in Containers.items():
-        if value.group == group or group=='':
+        if value.group == group or group == "":
             output = f"{key:<15}{value.group:<10}{value.description}"
             print(output)
 
@@ -323,7 +313,7 @@ def main() -> int:
     print("*********************************************************************")
     Containers = load_container_config_file(container_config)
 
-    if args.operation.lower() == 'list':
+    if args.operation.lower() == "list":
         # just list all detected containers then exit
         list_containers(Containers, args.group)
         return
@@ -337,10 +327,7 @@ def main() -> int:
         )
 
     apptainer_command = format_command(
-        args.operation,
-        model_name,
-        Containers[model_name],
-        args.cmd
+        args.operation, model_name, Containers[model_name], args.cmd
     )
     if args.debug:
         print("Debug enabled")
@@ -351,11 +338,14 @@ def main() -> int:
         try:
             proc.check_returncode()
         except subprocess.CalledProcessError as e:
-            print (f"An error occurred. Container exited with the exit code {e.returncode}:")
+            print(
+                f"An error occurred. Container exited with the exit code {e.returncode}:"
+            )
             return e.returncode
         return proc.returncode
-    #return code is used by pytest to check code ran successfully
+    # return code is used by pytest to check code ran successfully
     return 0
+
 
 if __name__ == "__main__":
     return_code = main()
--- util_functions.py
+++ util_functions.py
@@ -2,12 +2,13 @@ import os
 from pathlib import Path
 import pytest
 
-def check_test_output(golden_file,pytest_output):
-    '''
-    Function to check that the stdout and stderr captured 
+
+def check_test_output(golden_file, pytest_output):
+    """
+    Function to check that the stdout and stderr captured
     by pytest matches some known good output.
 
-    '''
+    """
     # env variable to control if we want to use current output as new benchmark
 
     UPDATE_TESTS = os.getenv("UPDATE_TESTS")
@@ -20,21 +21,22 @@ def check_test_output(golden_file,pytest_output):
     expected_output = golden_file.read_text()
     assert pytest_output == expected_output
 
-def create_build_options(options:dict)->str:
-    ''' 
+
+def create_build_options(options: dict) -> str:
+    """
     function to create a build options string for Apptainer from a dict
     The returned string is of the form "--build-arg KEY1=VALUE1 -build-arg KEY2=VALUE2 ..."
 
-    This is used by Apptainer with the build command to allow the use of placeholders 
-    using {{ variable }} syntax in the container definition files. 
+    This is used by Apptainer with the build command to allow the use of placeholders
+    using {{ variable }} syntax in the container definition files.
     (see https://apptainer.org/docs/user/main/cli/apptainer_build.html for more details).
-    
+
     This allows us to use a single definition file to cover multiple pre-trained models
-    from the same family and ultimately saves us using multiple definition files with 
-    only tiny differences.  
-    '''
+    from the same family and ultimately saves us using multiple definition files with
+    only tiny differences.
+    """
     if options:
-        build_args_str=''.join([f' --build-arg {k}={options[k]}' for k in options])
+        build_args_str = "".join([f" --build-arg {k}={options[k]}" for k in options])
     else:
-        build_args_str=''
+        build_args_str = ""
     return build_args_str
