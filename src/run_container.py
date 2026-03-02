379b9e32500b4bd063362174b452b29c3f7189ab
Ben Thorpe <ben.thorpe@york.ac.uk>
2026-03-02 14:49:56 +0000

    yet more fixes for bede

--- src/run_container.py
+++ src/run_container.py
@@ -140,7 +140,9 @@ def check_container_config(config_files: list):
                 result.container_definition = check_container_def(
                     result.container_definition
                 )
-
+            # Make parent directory(s) of definition file if it does not exist
+            Path(result.container_definition).mkdir(parents=True, exist_ok=True)
+            
             # do some checks for shared directory if defined
             if result.shared_directories != []:
                 for directory in result.shared_directories:
